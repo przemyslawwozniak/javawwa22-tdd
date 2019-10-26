49b27a1963004fcb2ace175072a4d32fd18fd2b1
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-10-26 15:49:36 +0200

    AssertJ

---
+++ src/main/java/pl/sda/javawwa22/fluenttesting/User.java
@@ -0,0 +1,39 @@
+package pl.sda.javawwa22.fluenttesting;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class User {
+
+    String username;
+    LocalDateTime lastLoggedIn;
+    LocalDateTime registered;
+    UserStatus status;
+
+    public void printLastLoggedIn() {
+        System.out.println(getLastLoggedInText());
+    }
+
+    String getLastLoggedInText() {
+        return username + "@" + lastLoggedIn;
+    }
+
+    public long timeSinceLastLoggedIn() {
+        return calcTimeDiffInMinutes(lastLoggedIn);
+    }
+
+    public long timeSinceRegistered() {
+        return calcTimeDiffInMinutes(registered);
+    }
+
+    private long calcTimeDiffInMinutes(LocalDateTime dateTime) {
+        return dateTime.until(LocalDateTime.now(), ChronoUnit.MINUTES);
+    }
+
+    public enum UserStatus {
+        INITIALIZED,
+        ACTIVATED,
+        DEACTIVATED
+    }
+
+}
---
+++ src/test/java/pl/sda/javawwa22/fluenttesting/UserTest.java
@@ -0,0 +1,7 @@
+package pl.sda.javawwa22.fluenttesting;
+
+public class UserTest {
+
+
+
+}
