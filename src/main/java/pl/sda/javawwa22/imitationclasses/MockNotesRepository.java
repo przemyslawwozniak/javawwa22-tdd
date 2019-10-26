c0ec803d2c5114e011fd6c2e8f450266c6e1e52b
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-10-26 12:33:33 +0200

    MockNotesRepository delivered

--- src/main/java/pl/sda/javawwa22/imitationclasses/MockNotesRepository.java
+++ src/main/java/pl/sda/javawwa22/imitationclasses/MockNotesRepository.java
@@ -1,23 +1,41 @@
 package pl.sda.javawwa22.imitationclasses;
 
-import java.util.List;
+import java.util.*;
 
 public class MockNotesRepository implements NotesRepository {
 
-
+    //symulujemy 'baze danych' w pamieci - potrzebujemy kontenera na dane typu
+    //fullName - List<Note>
+    private Map<String, List<Note>> nameToNotesMap = new HashMap<>();
 
     @Override
     public void save(Note note) {
-
+        List<Note> notes;
+        if(nameToNotesMap.containsKey(note.getFullName())) {
+            //druga i kolejna ocena
+            notes = nameToNotesMap.get(note.getFullName());
+        }
+        else {
+            //pierwsza ocena tego goscia
+            notes = new ArrayList<>();
+        }
+        notes.add(note);
+        nameToNotesMap.put(note.getFullName(), notes);
     }
 
+    //zwracamy pusta liste jesli nie ma takiego studenta
     @Override
     public List<Note> getAllNotesOf(String fullName) {
-        return null;
+        if(nameToNotesMap.containsKey(fullName)) {
+            return nameToNotesMap.get(fullName);
+        }
+        else {
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public void removeAll() {
-
+        nameToNotesMap.clear();
     }
 }
