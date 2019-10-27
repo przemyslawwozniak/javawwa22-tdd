4d03a3dc2c191e044452e154ad73e958181bfc9b
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-10-27 13:03:15 +0100

    TicTacToe TDD board prints

--- src/main/java/pl/sda/javawwa22/tictactoe/Board.java
+++ src/main/java/pl/sda/javawwa22/tictactoe/Board.java
@@ -14,9 +14,38 @@ public class Board {
         Preconditions.checkNotNull(sign);
         Preconditions.checkArgument(sign.equals(currentSign),
                 "Now is the turn of the player %s", currentSign.name());
+        Preconditions.checkArgument(position >= 0 && position <= 8,
+                "Only positions 0-8 can be taken, not %d", position);
+        Preconditions.checkArgument(signs[position] == null,
+                "Position %d already taken", position);
 
         signs[position] = sign;
         currentSign = currentSign.reverse();
+    }
+
+    public void printBoard() {
+        System.out.println(getPrintableBoard());
+    }
+
+    String getPrintableBoard() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < signs.length; i++) {
+            if(signs[i] == null) {
+                stringBuilder.append(i);
+            }
+            else {
+                stringBuilder.append(signs[i].name());
+            }
+            if(i == 2 || i == 5 || i == 8) {
+                stringBuilder.append("\n");
+            }
+            else {
+                stringBuilder.append("|");
+            }
+        }
+
+        return stringBuilder.toString();
     }
 
     public enum Sign {
--- src/test/java/pl/sda/javawwa22/tictactoe/TicTacToeTest.java
+++ src/test/java/pl/sda/javawwa22/tictactoe/TicTacToeTest.java
@@ -2,6 +2,7 @@ package pl.sda.javawwa22.tictactoe;
 
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TicTacToeTest {
@@ -48,6 +49,92 @@ public class TicTacToeTest {
         playerO.placeSign(board, 4);    //throws here...
         //then:
         //...throws
+    }
+
+    @Test
+    public void players_move_in_legal_sequence() {
+        //given:
+
+        //when:
+        playerX.placeSign(board, 4);
+        playerO.placeSign(board, 3);
+        playerX.placeSign(board, 2);
+        playerO.placeSign(board, 1);
+
+        //then:
+        Assertions.assertThat(board.signs)
+                .contains(null, Board.Sign.O, Board.Sign.X,
+                        Board.Sign.O, Board.Sign.X, null,
+                        null, null, null);
+    }
+
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void players_move_in_illegal_sequence() {
+        //given:
+
+        //when:
+        playerX.placeSign(board, 4);
+        playerX.placeSign(board, 3);
+
+        //then:
+        //throws
+    }
+
+    @Test(expectedExceptions = {IllegalArgumentException.class})
+    public void player_cannot_place_sign_on_already_taken_square() {
+        //given:
+
+        //when:
+        playerX.placeSign(board, 4);
+        playerO.placeSign(board, 3);
+        playerX.placeSign(board, 4);
+
+        //then:
+        //throws
+    }
+
+    @Test(dataProvider = "allowedPositions")
+    public void player_can_place_sign_on_positions_0_to_8(int position) {
+        playerX.placeSign(board, position);
+    }
+
+    @Test(dataProvider = "notAllowedPositions", expectedExceptions = {IllegalArgumentException.class})
+    public void player_cannot_place_sign_outside_of_the_board(int position) {
+        playerX.placeSign(board, position);
+    }
+
+    /*
+    0|X|2\n
+    O|X|X\n
+    6|O|8\n
+     */
+    @Test
+    public void prints_board() {
+        playerX.placeSign(board, 4);
+        playerO.placeSign(board, 3);
+        playerX.placeSign(board, 1);
+        playerO.placeSign(board, 7);
+        playerX.placeSign(board, 5);
+
+        board.printBoard();
+
+        //then:
+        Assertions.assertThat(board.getPrintableBoard())
+                .isEqualTo("0|X|2\nO|X|X\n6|O|8\n");
+    }
+
+    @DataProvider(name = "allowedPositions")
+    private Object[][] getAllowedPositions() {
+        return new Object[][] {
+                {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}
+        };
+    }
+
+    @DataProvider(name = "notAllowedPositions")
+    private Object[][] getNotAllowedPositions() {
+        return new Object[][] {
+                {-1}, {-5}, {9}, {99}, {13}, {11}, {-100}
+        };
     }
 
 }
